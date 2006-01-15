173714c64994c083398ae273e3861331aa854945
gwenn <gtreguier@gmail.com>
2006-01-15 13:15:08 +0000

    Repayments optimized with Permutations.

--- test/tc_apportioner.rb
+++ test/tc_apportioner.rb
@@ -14,62 +14,61 @@ module TestKitty
     end
 
     def test_distribute_with_empty_balances
+      creditors = nil
       repayments = nil
+      debitors = nil
       assert_nothing_raised {
-        repayments = @apportioner.distribute(@balances)
+        creditors, repayments, debitors = @apportioner.distribute(@balances)
       }
-      assert(repayments.empty?)
+      assert_nil(creditors)
+      assert_nil(repayments)
+      assert_nil(debitors)
     end
 
     def test_distribute
-      @balances[@person0] = 5.00
-      @balances[@person1] = -5.00
-      repayments = @apportioner.distribute(@balances)
+      @balances[@person0] = 5
+      @balances[@person1] = -5
+      creditors, repayments, debitors = @apportioner.distribute(@balances)
+      assert_equal(1, creditors.length)
+      assert_equal(@person0, creditors[0])
+      assert_equal(1, debitors.length)
+      assert_equal(@person1, debitors[0])
       assert_equal(1, repayments.length)
-      assert_equal(@person1, repayments[0][0])
-      assert_equal(@person0, repayments[0][1])
-      assert_in_delta(5.00, repayments[0][2], 0.001)
+      assert_equal([[5]], repayments[0])
     end
 
     def test_distribute_order
-      @balances[@person0] = 25.00
-      @balances[@person1] = 10.00
-      @balances[@person2] = -15.00
-      @balances[@person3] = -20.00
-      repayments = @apportioner.distribute(@balances)
-      assert_equal(3, repayments.length)
-      assert_equal(@person2, repayments[0][0])
-      assert_equal(@person0, repayments[0][1])
-      assert_in_delta(15.00, repayments[0][2], 0.001)
-
-      assert_equal(@person3, repayments[1][0])
-      assert_equal(@person0, repayments[1][1])
-      assert_in_delta(10.00, repayments[1][2], 0.001)
-
-      assert_equal(@person3, repayments[2][0])
-      assert_equal(@person1, repayments[2][1])
-      assert_in_delta(10.00, repayments[2][2], 0.001)
+      @balances[@person0] = 25
+      @balances[@person1] = 10
+      @balances[@person2] = -15
+      @balances[@person3] = -20
+      creditors, repayments, debitors = @apportioner.distribute(@balances)
+      assert_equal(2, creditors.length)
+      assert_equal(@person0, creditors[0])
+      assert_equal(@person1, creditors[1])
+      assert_equal(2, debitors.length)
+      assert_equal(@person2, debitors[0])
+      assert_equal(@person3, debitors[1])
+      assert_equal(2, repayments.length)
+      assert_equal([[15, 10], [0, 10]], repayments[0])
+      assert_equal([[5, 20], [10, 0]], repayments[1])
     end
 
     def test_distribute_with_prefer_to_pay_back
       @person2.prefer_to_pay_back(@person1)
-      @balances[@person0] = 15.00
-      @balances[@person1] = 15.00
-      @balances[@person2] = -10.00
-      @balances[@person3] = -20.00
-      repayments = @apportioner.distribute(@balances)
-      assert_equal(3, repayments.length)
-      assert_equal(@person2, repayments[0][0])
-      assert_equal(@person1, repayments[0][1])
-      assert_in_delta(10.00, repayments[0][2], 0.001)
-
-      assert_equal(@person3, repayments[1][0])
-      assert_equal(@person0, repayments[1][1])
-      assert_in_delta(15.00, repayments[1][2], 0.001)
-
-      assert_equal(@person3, repayments[2][0])
-      assert_equal(@person1, repayments[2][1])
-      assert_in_delta(5.00, repayments[2][2], 0.001)
+      @balances[@person0] = 15
+      @balances[@person1] = 15
+      @balances[@person2] = -10
+      @balances[@person3] = -20
+      creditors, repayments, debitors = @apportioner.distribute(@balances)
+      assert_equal(2, creditors.length)
+      assert_equal(@person0, creditors[0])
+      assert_equal(@person1, creditors[1])
+      assert_equal(2, debitors.length)
+      assert_equal(@person2, debitors[0])
+      assert_equal(@person3, debitors[1])
+      assert_equal(1, repayments.length)
+      assert_equal([[0, 15], [10, 5]], repayments[0])
     end
     
     def teardown
@@ -77,6 +76,11 @@ module TestKitty
       @balances = nil
       @person1 = nil
       @person0 = nil
+    end
+
+    private
+    def assert_amount(expected, actual)
+      assert(Kitty::Payment.to_i(expected), actual)
     end
   end
 end
